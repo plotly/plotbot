abourget/slick

The OAuth2 authentication.
Slack API with bot credentials now available at `bot.Slack`.

bot.go
@@ -21,7 +21,7 @@ type Bot struct {
 	Config     SlackConfig
 
 	// Slack connectivity
-	api      *slack.Client
+	Slack    *slack.Client
 	rtm      *slack.RTM
 	Users    map[string]slack.User
 	Channels map[string]slack.Channel
@@ -109,10 +109,10 @@ func (bot *Bot) Run() {
 		go bot.WebServer.RunServer()
 	}
 
-	bot.api = slack.New(bot.Config.ApiToken)
-	bot.api.SetDebug(bot.Config.Debug)
+	bot.Slack = slack.New(bot.Config.ApiToken)
+	bot.Slack.SetDebug(bot.Config.Debug)
 
-	rtm := bot.api.NewRTM()
+	rtm := bot.Slack.NewRTM()
 	bot.rtm = rtm
 
 	bot.setupHandlers()
@@ -366,7 +366,7 @@ func (bot *Bot) handleRTMEvent(event *slack.SlackEvent) {
 		for _, channelName := range bot.Config.JoinChannels {
 			channel := bot.GetChannelByName(channelName)
 			if channel != nil && !channel.IsMember {
-				bot.api.JoinChannel(channel.Id)
+				bot.Slack.JoinChannel(channel.Id)
 			}
 		}
 

plotly/plotbot

(cherry picked from commit 7a97ce693bf679b83fc1fd921732f0f6a3f1e26f)
Conflicts:
	bot.go
	webauth/auth.go

bot.go
@@ -21,7 +21,7 @@ type Bot struct {
 	Config     SlackConfig
 
 	// Slack connectivity
-	api      *slack.Slack
+	Slack    *slack.Slack
 	ws       *slack.SlackWS
 	Users    map[string]slack.User
 	Channels map[string]slack.Channel
@@ -196,7 +196,7 @@ func (bot *Bot) ReplyPrivately(msg *Message, reply string) {
 
 func (bot *Bot) Notify(room, color, format, msg string, notify bool) error {
 	log.Println("DEPRECATED. Please use the Slack API with .PostMessage")
-	// bot.api.PostMessage(room, msg, slack.PostMessageParameters{
+	// bot.Slack.PostMessage(room, msg, slack.PostMessageParameters{
 	// 	Attachments: []slack.Attachment{
 	// 		{
 	// 			Color: color,
@@ -230,16 +230,16 @@ func (bot *Bot) connectClient() (err error) {
 		resource = "bot"
 	}
 
-	bot.api = slack.New(bot.Config.ApiToken)
-	//bot.api.SetDebug(true)
+	bot.Slack = slack.New(bot.Config.ApiToken)
+	//bot.Slack.SetDebug(true)
 
-	ws, err := bot.api.StartRTM("", "http://safeidentity.slack.com")
+	ws, err := bot.Slack.StartRTM("", "http://safeidentity.slack.com")
 	if err != nil {
 		return err
 	}
 	bot.ws = ws
 
-	infos := bot.api.GetInfo()
+	infos := bot.Slack.GetInfo()
 	bot.Myself = infos.User
 	bot.cacheUsers(infos.Users)
 	bot.cacheChannels(infos.Channels, infos.Groups)
@@ -247,7 +247,7 @@ func (bot *Bot) connectClient() (err error) {
 	for _, channelName := range bot.Config.JoinChannels {
 		channel := bot.GetChannelByName(channelName)
 		if channel != nil {
-			bot.api.JoinChannel(channel.Id)
+			bot.Slack.JoinChannel(channel.Id)
 		}
 	}
 