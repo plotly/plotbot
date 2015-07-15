abourget/slick

Changed API footprint for initialisation.
Added "IsEdition".

bugger/bugger.go
@@ -52,7 +52,7 @@ func (bugger *Bugger) makeBugReporter(days int) (reporter bugReporter) {
 	return
 }
 
-func (bugger *Bugger) InitChatPlugin(bot *slick.Bot) {
+func (bugger *Bugger) InitPlugin(bot *slick.Bot) {
 
 	/*
 	 * Get an array of issues matching Filters

deployer/deployer.go
@@ -41,7 +41,7 @@ func init() {
 	slick.RegisterPlugin(&Deployer{})
 }
 
-func (dep *Deployer) InitChatPlugin(bot *slick.Bot) {
+func (dep *Deployer) InitPlugin(bot *slick.Bot) {
 	var conf struct {
 		Deployer DeployerConfig
 	}

mooder/mooder.go
@@ -15,7 +15,7 @@ func init() {
 	slick.RegisterPlugin(&Mooder{})
 }
 
-func (mooder *Mooder) InitChatPlugin(bot *slick.Bot) {
+func (mooder *Mooder) InitPlugin(bot *slick.Bot) {
 	mooder.bot = bot
 	go mooder.SetupMoodChanger()
 }

plotberry/plotberry.go
@@ -26,7 +26,7 @@ func init() {
 	slick.RegisterPlugin(&PlotBerry{})
 }
 
-func (plotberry *PlotBerry) InitChatPlugin(bot *slick.Bot) {
+func (plotberry *PlotBerry) InitPlugin(bot *slick.Bot) {
 
 	plotberry.bot = bot
 	plotberry.celebrated = true

standup/standup.go
@@ -23,7 +23,7 @@ func init() {
 	slick.RegisterPlugin(&Standup{})
 }
 
-func (standup *Standup) InitChatPlugin(bot *slick.Bot) {
+func (standup *Standup) InitPlugin(bot *slick.Bot) {
 	standup.bot = bot
 	standup.sectionUpdates = make(chan sectionUpdate, 15)
 

totw/totw.go
@@ -16,7 +16,7 @@ func init() {
 	slick.RegisterPlugin(&Totw{})
 }
 
-func (totw *Totw) InitChatPlugin(bot *slick.Bot) {
+func (totw *Totw) InitPlugin(bot *slick.Bot) {
 	slick.RegisterStringList("useless techs", []string{
 		"http://i.minus.com/ib2bUNs2W1CI1V.gif",
 		"http://media.giphy.com/media/anl0wydLNhKus/giphy.gif",

wicked/wicked.go
@@ -32,7 +32,7 @@ func init() {
 	slick.RegisterPlugin(&Wicked{})
 }
 
-func (wicked *Wicked) InitChatPlugin(bot *slick.Bot) {
+func (wicked *Wicked) InitPlugin(bot *slick.Bot) {
 	wicked.bot = bot
 	wicked.meetings = make(map[string]*Meeting)
 

plotly/plotbot

(cherry picked from commit 835f54228f3e54886167a7e2be4989b4db9ef898)
Conflicts:
	bugger/bugger.go
	deployer/deployer.go
	funny/funny.go
	healthy/healthy.go
	mooder/mooder.go
	plotberry/plotberry.go
	standup/standup.go
	totw/totw.go
	wicked/wicked.go

bugger/bugger.go
@@ -52,7 +52,7 @@ func (bugger *Bugger) makeBugReporter(days int) (reporter bugReporter) {
 	return
 }
 
-func (bugger *Bugger) InitChatPlugin(bot *plotbot.Bot) {
+func (bugger *Bugger) InitPlugin(bot *plotbot.Bot) {
 
 	/*
 	 * Get an array of issues matching Filters

deployer/deployer.go
@@ -41,7 +41,7 @@ func init() {
 	plotbot.RegisterPlugin(&Deployer{})
 }
 
-func (dep *Deployer) InitChatPlugin(bot *plotbot.Bot) {
+func (dep *Deployer) InitPlugin(bot *plotbot.Bot) {
 	var conf struct {
 		Deployer DeployerConfig
 	}

mooder/mooder.go
@@ -15,7 +15,7 @@ func init() {
 	plotbot.RegisterPlugin(&Mooder{})
 }
 
-func (mooder *Mooder) InitChatPlugin(bot *plotbot.Bot) {
+func (mooder *Mooder) InitPlugin(bot *plotbot.Bot) {
 	mooder.bot = bot
 	go mooder.SetupMoodChanger()
 }

plotberry/plotberry.go
@@ -33,7 +33,7 @@ func init() {
 	plotbot.RegisterPlugin(&PlotBerry{})
 }
 
-func (plotberry *PlotBerry) InitChatPlugin(bot *plotbot.Bot) {
+func (plotberry *PlotBerry) InitPlugin(bot *plotbot.Bot) {
 
 	var conf struct {
 		Plotberry PlotberryConf

standup/standup.go
@@ -23,7 +23,7 @@ func init() {
 	plotbot.RegisterPlugin(&Standup{})
 }
 
-func (standup *Standup) InitChatPlugin(bot *plotbot.Bot) {
+func (standup *Standup) InitPlugin(bot *plotbot.Bot) {
 	standup.bot = bot
 	standup.sectionUpdates = make(chan sectionUpdate, 15)
 

totw/totw.go
@@ -16,7 +16,7 @@ func init() {
 	plotbot.RegisterPlugin(&Totw{})
 }
 
-func (totw *Totw) InitChatPlugin(bot *plotbot.Bot) {
+func (totw *Totw) InitPlugin(bot *plotbot.Bot) {
 	plotbot.RegisterStringList("useless techs", []string{
 		"http://i.minus.com/ib2bUNs2W1CI1V.gif",
 		"http://media.giphy.com/media/anl0wydLNhKus/giphy.gif",

wicked/wicked.go
@@ -32,7 +32,7 @@ func init() {
 	plotbot.RegisterPlugin(&Wicked{})
 }
 
-func (wicked *Wicked) InitChatPlugin(bot *plotbot.Bot) {
+func (wicked *Wicked) InitPlugin(bot *plotbot.Bot) {
 	wicked.bot = bot
 	wicked.meetings = make(map[string]*Meeting)
 