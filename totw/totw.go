abourget/slick

Migrated from Hipchat to Slack.
"example-bot" builds with all plugins.

Moved logic to write PID inside the bot.

All tests pass.

TODO:
 * rework how plugins hook into the bot
 * rewrite the Web UI with polymer 0.9, having a web-plugin mechanism for
   each Go plugin
 * rethink the auth for the web UI
 * expose the Slack "api" to plugins.. so they can emit attachments, etc..
 * expose all the event stream to plugins... so they can react too..
 * expose "sub-messages" to plugins in an appealing way
 * phase out the "tabula rasa" plugin ?
 * flag when messages are @everyone @channel or @group .. so we can filter
   those.
 * improve the filtering system, to handle those things
   * only in groups, etc..
 * webui: get rid of all the gulp build steps.. provide only riced-in
   polymer things..
 * some other things I forgot..

totw/totw.go
@@ -45,7 +45,7 @@ func (totw *Totw) InitChatPlugin(bot *slick.Bot) {
 
 	totw.bot = bot
 
-	go totw.ScheduleAlerts(bot.Config.TeamRoom, time.Thursday, 16, 0)
+	go totw.ScheduleAlerts(bot.Config.GeneralChannel, time.Thursday, 16, 0)
 
 	bot.ListenFor(&slick.Conversation{
 		HandlerFunc: totw.ChatHandler,
@@ -53,19 +53,19 @@ func (totw *Totw) InitChatPlugin(bot *slick.Bot) {
 }
 
 func (totw *Totw) ChatHandler(conv *slick.Conversation, msg *slick.Message) {
-	if strings.HasPrefix(msg.Body, "!totw") || strings.HasPrefix(msg.Body, "!techoftheweek") {
+	if strings.HasPrefix(msg.Text, "!totw") || strings.HasPrefix(msg.Text, "!techoftheweek") {
 		conv.ReplyMention(msg, slick.RandomString("tech adept"))
 	}
 }
 
-func (totw *Totw) ScheduleAlerts(room string, w time.Weekday, hour, min int) {
+func (totw *Totw) ScheduleAlerts(channel string, w time.Weekday, hour, min int) {
 	for {
 		next, when := slick.NextWeekdayTime(w, hour, min)
 		log.Println("TOTW: Next occurrence: ", next)
 
 		<-time.After(when)
 
-		totw.bot.SendToRoom(room, slick.RandomString("useless techs"))
-		totw.bot.SendToRoom(room, `Time for some Tech of the Week! What's your pick ?  Start your line with "!techoftheweek"`)
+		totw.bot.SendToChannel(channel, slick.RandomString("useless techs"))
+		totw.bot.SendToChannel(channel, `Time for some Tech of the Week! What's your pick ?  Start your line with "!techoftheweek"`)
 	}
 }

wicked/wicked_test.go
@@ -4,13 +4,14 @@ import (
 	"testing"
 
 	"github.com/abourget/slick"
+	"github.com/nlopes/slack"
 )
 
 func TestFindNextRoom(t *testing.T) {
 	w := &Wicked{
-		bot: &slick.Bot{Rooms: []slick.Room{
-			slick.Room{JID: "room2"},
-			slick.Room{JID: "room3"},
+		bot: &slick.Bot{Channels: map[string]slack.Channel{
+			"room2": {BaseChannel: slack.BaseChannel{Id: "room2"}, Name: "room2"},
+			"room3": {BaseChannel: slack.BaseChannel{Id: "room3"}, Name: "room3"},
 		}},
 		meetings: map[string]*Meeting{
 			"room1": &Meeting{},
@@ -27,7 +28,7 @@ func TestFindNextRoom(t *testing.T) {
 	if res == nil {
 		t.Fail()
 	}
-	if res.JID != "room2" {
+	if res.Id != "room2" {
 		t.Error(`Should be "room2"`)
 	}
 
@@ -36,15 +37,15 @@ func TestFindNextRoom(t *testing.T) {
 	if res == nil {
 		t.Fail()
 	}
-	if res.JID != "room3" {
+	if res.Id != "room3" {
 		t.Error(`Should be "room3"`)
 	}
 }
 
 func TestFindNextRoomNilFromRoom(t *testing.T) {
 	w := &Wicked{
-		bot: &slick.Bot{Rooms: []slick.Room{
-			slick.Room{JID: "room1"},
+		bot: &slick.Bot{Channels: map[string]slack.Channel{
+			"room1": {BaseChannel: slack.BaseChannel{Id: "room1"}, Name: "room1"},
 		}},
 		meetings:  map[string]*Meeting{},
 		confRooms: []string{"room1"},
@@ -55,14 +56,14 @@ func TestFindNextRoomNilFromRoom(t *testing.T) {
 	if res == nil {
 		t.Fail()
 	}
-	if res.JID != "room1" {
+	if res.Id != "room1" {
 		t.Error(`Should be "room1"`)
 	}
 }
 
 func TestFindNextRoomAllTake(t *testing.T) {
 	w := &Wicked{
-		bot: &slick.Bot{Rooms: []slick.Room{}},
+		bot: &slick.Bot{Channels: map[string]slack.Channel{}},
 		meetings: map[string]*Meeting{
 			"room1": &Meeting{},
 		},

plotly/plotbot

Cherry-picked from 3691c6439cb32e4733b7c4d46895e518a071961f
Conflicts:
	bot.go
	bot_test.go
	example-bot/main.go
	healthy/healthy.go
	plotberry/plotberry.go
	rooms.go
	standup/standup.go
	standup/standupData_test.go
	standup/standupUser.go
	standup/standupUser_test.go
	totw/totw.go
	user.go
	webutils/webutils.go
	wicked/meeting.go
	wicked/wicked.go
	wicked/wicked_test.go

totw/totw.go
@@ -45,7 +45,7 @@ func (totw *Totw) InitChatPlugin(bot *plotbot.Bot) {
 
 	totw.bot = bot
 
-	go totw.ScheduleAlerts(bot.Config.TeamRoom, time.Thursday, 16, 0)
+	go totw.ScheduleAlerts(bot.Config.GeneralChannel, time.Thursday, 16, 0)
 
 	bot.ListenFor(&plotbot.Conversation{
 		HandlerFunc: totw.ChatHandler,
@@ -53,19 +53,19 @@ func (totw *Totw) InitChatPlugin(bot *plotbot.Bot) {
 }
 
 func (totw *Totw) ChatHandler(conv *plotbot.Conversation, msg *plotbot.Message) {
-	if strings.HasPrefix(msg.Body, "!totw") || strings.HasPrefix(msg.Body, "!techoftheweek") {
+	if strings.HasPrefix(msg.Text, "!totw") || strings.HasPrefix(msg.Text, "!techoftheweek") {
 		conv.ReplyMention(msg, plotbot.RandomString("tech adept"))
 	}
 }
 
-func (totw *Totw) ScheduleAlerts(room string, w time.Weekday, hour, min int) {
+func (totw *Totw) ScheduleAlerts(channel string, w time.Weekday, hour, min int) {
 	for {
 		next, when := plotbot.NextWeekdayTime(w, hour, min)
 		log.Println("TOTW: Next occurrence: ", next)
 
 		<-time.After(when)
 
-		totw.bot.SendToRoom(room, plotbot.RandomString("useless techs"))
-		totw.bot.SendToRoom(room, `Time for some Tech of the Week! What's your pick ?  Start your line with "!techoftheweek"`)
+		totw.bot.SendToChannel(channel, plotbot.RandomString("useless techs"))
+		totw.bot.SendToChannel(channel, `Time for some Tech of the Week! What's your pick ?  Start your line with "!techoftheweek"`)
 	}
 }

wicked/wicked_test.go
@@ -4,13 +4,14 @@ import (
 	"testing"
 
 	"github.com/plotly/plotbot"
+	"github.com/nlopes/slack"
 )
 
 func TestFindNextRoom(t *testing.T) {
 	w := &Wicked{
-		bot: &plotbot.Bot{Rooms: []plotbot.Room{
-			plotbot.Room{JID: "room2"},
-			plotbot.Room{JID: "room3"},
+		bot: &slick.Bot{Channels: map[string]slack.Channel{
+			"room2": {BaseChannel: slack.BaseChannel{Id: "room2"}, Name: "room2"},
+			"room3": {BaseChannel: slack.BaseChannel{Id: "room3"}, Name: "room3"},
 		}},
 		meetings: map[string]*Meeting{
 			"room1": &Meeting{},
@@ -27,7 +28,7 @@ func TestFindNextRoom(t *testing.T) {
 	if res == nil {
 		t.Fail()
 	}
-	if res.JID != "room2" {
+	if res.Id != "room2" {
 		t.Error(`Should be "room2"`)
 	}
 
@@ -36,15 +37,15 @@ func TestFindNextRoom(t *testing.T) {
 	if res == nil {
 		t.Fail()
 	}
-	if res.JID != "room3" {
+	if res.Id != "room3" {
 		t.Error(`Should be "room3"`)
 	}
 }
 
 func TestFindNextRoomNilFromRoom(t *testing.T) {
 	w := &Wicked{
-		bot: &plotbot.Bot{Rooms: []plotbot.Room{
-			plotbot.Room{JID: "room1"},
+		bot: &plotbot.Bot{Channels: map[string]slack.Channel{
+			"room1": {BaseChannel: slack.BaseChannel{Id: "room1"}, Name: "room1"},
 		}},
 		meetings:  map[string]*Meeting{},
 		confRooms: []string{"room1"},
@@ -55,14 +56,14 @@ func TestFindNextRoomNilFromRoom(t *testing.T) {
 	if res == nil {
 		t.Fail()
 	}
-	if res.JID != "room1" {
+	if res.Id != "room1" {
 		t.Error(`Should be "room1"`)
 	}
 }
 
 func TestFindNextRoomAllTake(t *testing.T) {
 	w := &Wicked{
-		bot: &plotbot.Bot{Rooms: []plotbot.Room{}},
+		bot: &plotbot.Bot{Channels: map[string]slack.Channel{}},
 		meetings: map[string]*Meeting{
 			"room1": &Meeting{},
 		},