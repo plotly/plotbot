abourget/slick

Reworked Web and authentication, made it an optional plugin.
You'll want to have your own handler for a web app now.

Authentication is untested, as I don,t have an OAuth2-enabled Slack account.

example-bot/main.go
@@ -15,6 +15,7 @@ import (
 	_ "github.com/abourget/slick/standup"
 	_ "github.com/abourget/slick/totw"
 	_ "github.com/abourget/slick/web"
+	_ "github.com/abourget/slick/webauth"
 	_ "github.com/abourget/slick/webutils"
 	_ "github.com/abourget/slick/wicked"
 )

plugin.go
@@ -1,6 +1,13 @@
 package slick
 
-import "github.com/gorilla/mux"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"github.com/abourget/slack"
+)
 
 //
 // Bot plugins
@@ -13,15 +20,29 @@ type PluginInitializer interface {
 }
 
 type WebServer interface {
+	// Used internally by the `slick` library.
 	InitWebServer(*Bot, []string)
-	ServeWebRequests()
+	RunServer()
+
+	// Used by an Auth provider.
+	SetAuthMiddleware(func(http.Handler) http.Handler)
+	SetAuthenticatedUserFunc(func(req *http.Request) (*slack.User, error))
+
+	// Can be called by any plugins.
 	PrivateRouter() *mux.Router
 	PublicRouter() *mux.Router
+	GetSession(*http.Request) *sessions.Session
+	AuthenticatedUser(*http.Request) (*slack.User, error)
 }
 
 // WebPlugin initializes plugins with a `Bot` instance, a `privateRouter` and a `publicRouter`. All URLs handled by the `publicRouter` must start with `/public/`.
 type WebPlugin interface {
-	InitWebPlugin(*Bot, *mux.Router, *mux.Router)
+	InitWebPlugin(bot *Bot, private *mux.Router, public *mux.Router)
+}
+
+// WebServerAuth returns a middleware warpping the passed on `http.Handler`. Only one auth handler can be added.
+type WebServerAuth interface {
+	InitWebServerAuth(bot *Bot, webserver WebServer)
 }
 
 var registeredPlugins = make([]Plugin, 0)
@@ -56,9 +77,19 @@ func initWebPlugins(bot *Bot) {
 	}
 
 	for _, plugin := range registeredPlugins {
-		webPlugin, ok := plugin.(WebPlugin)
-		if ok {
+		if webPlugin, ok := plugin.(WebPlugin); ok {
 			webPlugin.InitWebPlugin(bot, bot.WebServer.PrivateRouter(), bot.WebServer.PublicRouter())
 		}
+
+		count := 0
+		if webServerAuth, ok := plugin.(WebServerAuth); ok {
+			count += 1
+
+			if count > 1 {
+				log.Fatalln("Can not load two WebServerAuth plugins. Already loaded one.")
+			}
+			webServerAuth.InitWebServerAuth(bot, bot.WebServer)
+		}
+
 	}
 }

web/user.go
@@ -1,29 +1,16 @@
 package web
 
 import (
-	"encoding/gob"
-	"html/template"
 	"encoding/json"
-	"log"
+	"html/template"
+
+	"github.com/abourget/slack"
 )
 
-type GoogleUserProfile struct {
-	Name          string `json:"name"`
-	Hd            string `json:"hd"`
-	Email         string `json:"email"`
-	Picture       string `json:"picture"`
-	VerifiedEmail bool   `json:"verified_email"`
-}
-
-func (up *GoogleUserProfile) AsJavascript() template.JS {
-	jsonProfile, err := json.MarshalIndent(up, "", "  ")
+func userAsJavascript(user *slack.User) template.JS {
+	jsonProfile, err := json.MarshalIndent(user, "", "  ")
 	if err != nil {
-		log.Fatal("Couldn't marshal GoogleUserProfile for rendering", err)
-		return template.JS("{}")
+		return template.JS(`{"error": "couldn't decode user"}`)
 	}
 	return template.JS(jsonProfile)
-}
-
-func init() {
-	gob.Register(&GoogleUserProfile{})
 }

web/webapp.go
@@ -7,33 +7,30 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/GeertJohan/go.rice"
+	"github.com/abourget/slack"
+	"github.com/abourget/slick"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/context"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
-	"github.com/abourget/slick"
-	"golang.org/x/oauth2"
 )
 
 var web *Webapp
 
 type Webapp struct {
-	config         *WebappConfig
-	store          *sessions.CookieStore
-	bot            *slick.Bot
-	handler        *negroni.Negroni
-	privateRouter  *mux.Router
-	publicRouter   *mux.Router
-	enabledPlugins []string
+	config                *WebappConfig
+	store                 *sessions.CookieStore
+	bot                   *slick.Bot
+	handler               *negroni.Negroni
+	privateRouter         *mux.Router
+	publicRouter          *mux.Router
+	enabledPlugins        []string
+	authMiddleware        func(http.Handler) http.Handler
+	authenticatedUserFunc func(req *http.Request) (*slack.User, error)
 }
 
 type WebappConfig struct {
 	Listen            string `json:"listen"`
-	RedirectURL       string `json:"oauth_base_url"`
-	ClientID          string `json:"client_id"`
-	ClientSecret      string `json:"client_secret"`
-	RestrictDomain    string `json:"restrict_domain"`
 	SessionAuthKey    string `json:"session_auth_key"`
 	SessionEncryptKey string `json:"session_encrypt_key"`
 }
@@ -55,9 +52,6 @@ func (webapp *Webapp) InitWebServer(bot *slick.Bot, enabledPlugins []string) {
 	webapp.privateRouter = mux.NewRouter()
 	webapp.publicRouter = mux.NewRouter()
 
-	configureWebapp(&conf.Webapp)
-
-	webapp.privateRouter.HandleFunc("/", webapp.handleRoot)
 	web = webapp
 }
 
@@ -68,14 +62,34 @@ func (webapp *Webapp) PublicRouter() *mux.Router {
 	return webapp.publicRouter
 }
 
-func (webapp *Webapp) ServeWebRequests() {
+// SetAuthMiddleware should be called once by a WebServerAuth plugin, if any.
+func (webapp *Webapp) SetAuthMiddleware(middleware func(http.Handler) http.Handler) {
+	webapp.authMiddleware = middleware
+}
+
+// SetAuthenticatedUserFunc should be called once by a WebServerAuth plugin, if any.
+func (webapp *Webapp) SetAuthenticatedUserFunc(f func(req *http.Request) (*slack.User, error)) {
+	webapp.authenticatedUserFunc = f
+}
+
+func (webapp *Webapp) AuthenticatedUser(req *http.Request) (*slack.User, error) {
+	if webapp.authenticatedUserFunc == nil {
+		return nil, fmt.Errorf("No WebServerAuth plugin registered any AuthenticatedUser func call")
+	}
+	return webapp.authenticatedUserFunc(req)
+}
+
+func (webapp *Webapp) RunServer() {
 	privMux := http.NewServeMux()
-	privMux.Handle("/static/", http.StripPrefix("/static", http.FileServer(rice.MustFindBox("static").HTTPBox())))
-	privMux.Handle("/", webapp.privateRouter)
+	privMux.Handle("/", webapp.PrivateRouter())
 
 	pubMux := http.NewServeMux()
-	pubMux.Handle("/public/", webapp.publicRouter)
-	pubMux.Handle("/", NewOAuthMiddleware(privMux))
+	pubMux.Handle("/public/", webapp.PublicRouter())
+	if webapp.authMiddleware != nil {
+		pubMux.Handle("/", webapp.authMiddleware(privMux))
+	} else {
+		pubMux.Handle("/", privMux)
+	}
 
 	webapp.handler = negroni.Classic()
 	webapp.handler.UseHandler(context.ClearHandler(pubMux))
@@ -83,25 +97,13 @@ func (webapp *Webapp) ServeWebRequests() {
 	webapp.handler.Run(webapp.config.Listen)
 }
 
-// func LaunchWebapp(b *slick.Bot) {
-
-// 	rt.HandleFunc("/send_notif", handleNotif)
-// 	rt.HandleFunc("/hipchat/users", handleGetUsers)
-
-// 	n.Run("localhost:8080")
-// }
-
-func configureWebapp(conf *WebappConfig) {
-	oauthCfg = &oauth2.Config{
-		ClientID:     conf.ClientID,
-		ClientSecret: conf.ClientSecret,
-		RedirectURL:  conf.RedirectURL + "/oauth2callback",
-		Scopes:       []string{"openid", "profile", "email", "https://www.googleapis.com/auth/userinfo.profile"},
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  "https://accounts.google.com/o/oauth2/auth",
-			TokenURL: "https://accounts.google.com/o/oauth2/token",
-		},
+func (webapp *Webapp) GetSession(r *http.Request) *sessions.Session {
+	sess, err := web.store.Get(r, "slick")
+	if err != nil {
+		log.Println("web/session: warn: unable to decode Session cookie: ", err)
 	}
+
+	return sess
 }
 
 func (webapp *Webapp) handleRoot(w http.ResponseWriter, r *http.Request) {
@@ -110,7 +112,7 @@ func (webapp *Webapp) handleRoot(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	profile, _ := checkAuth(r)
+	profile, _ := webapp.AuthenticatedUser(r)
 
 	tpl, err := getRootTemplate()
 	if err != nil {
@@ -121,29 +123,21 @@ func (webapp *Webapp) handleRoot(w http.ResponseWriter, r *http.Request) {
 	ctx := struct {
 		CurrentUser, EnabledPlugins template.JS
 	}{
-		profile.AsJavascript(),
+		userAsJavascript(profile),
 		webapp.getEnabledPluginsJS(),
 	}
 	tpl.Execute(w, ctx)
 }
 
 func getRootTemplate() (*template.Template, error) {
-	box, err := rice.FindBox("static")
-	if err != nil {
-		return nil, fmt.Errorf("Error finding static assets: %s", err)
-	}
-
-	rawTpl, err := box.String("index.html")
-	if err != nil {
-		return nil, fmt.Errorf("Error loading index.html: %s", err)
-	}
-
-	tpl, err := template.New("index.html").Parse(rawTpl)
-	if err != nil {
-		return nil, fmt.Errorf("Cannot parse index.html: %s", err)
-	}
-
-	return tpl, nil
+	return template.New("index.html").Parse(`
+<html><head></head><body>
+<script>
+USER = {{.CurrentUser}};
+ENABLED_PLUGINS = {{.EnabledPlugins}};
+</script>
+</body></html>
+`)
 }
 
 func (webapp *Webapp) getEnabledPluginsJS() template.JS {

webauth/auth.go
@@ -1,0 +1,157 @@
+package webauth
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/abourget/slack"
+	"github.com/abourget/slick"
+	"golang.org/x/oauth2"
+)
+
+func init() {
+	slick.RegisterPlugin(&OAuthPlugin{})
+}
+
+type OAuthPlugin struct {
+	config    OAuthConfig
+	webserver slick.WebServer
+}
+
+type OAuthConfig struct {
+	RedirectURL  string `json:"oauth_base_url"`
+	ClientID     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+}
+
+func (p *OAuthPlugin) InitWebServerAuth(bot *slick.Bot, webserver slick.WebServer) {
+	p.webserver = webserver
+
+	var config struct {
+		WebAuthConfig OAuthConfig
+	}
+	bot.LoadConfig(&config)
+
+	conf := config.WebAuthConfig
+	webserver.SetAuthMiddleware(func(handler http.Handler) http.Handler {
+		return &OAuthMiddleware{
+			handler:   handler,
+			plugin:    p,
+			webserver: webserver,
+			bot:       bot,
+			oauthCfg: &oauth2.Config{
+				ClientID:     conf.ClientID,
+				ClientSecret: conf.ClientSecret,
+				RedirectURL:  conf.RedirectURL + "/oauth2callback",
+				Scopes:       []string{"identify"},
+				Endpoint: oauth2.Endpoint{
+					AuthURL:  "https://slack.com/oauth/authorize",
+					TokenURL: "https://slack.com/api/oauth.access",
+				},
+			},
+		}
+	})
+}
+
+func (p *OAuthPlugin) AuthenticatedUser(r *http.Request) (*slack.User, error) {
+	sess := p.webserver.GetSession(r)
+
+	rawProfile, ok := sess.Values["profile"]
+	if ok == false {
+		return nil, fmt.Errorf("Not authenticated")
+	}
+	profile, ok := rawProfile.(*slack.User)
+	if ok == false {
+		return nil, fmt.Errorf("Profile data unreadable")
+	}
+	return profile, nil
+}
+
+type OAuthMiddleware struct {
+	handler   http.Handler
+	plugin    *OAuthPlugin
+	webserver slick.WebServer
+	oauthCfg  *oauth2.Config
+	bot       *slick.Bot
+}
+
+func (mw *OAuthMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/oauth2callback" {
+		mw.handleOAuth2Callback(w, r)
+		return
+	}
+
+	_, err := mw.plugin.AuthenticatedUser(r)
+	if err != nil {
+		if r.URL.Path == "/" {
+			log.Println("Not logged in", err)
+			url := mw.oauthCfg.AuthCodeURL("", oauth2.SetAuthURLParam("team", mw.bot.Config.TeamDomain))
+			http.Redirect(w, r, url, http.StatusFound)
+		} else {
+			w.WriteHeader(http.StatusForbidden)
+		}
+		return
+	}
+
+	// Check if session exists, yield a 403 unless we're on the main page
+	mw.handler.ServeHTTP(w, r)
+}
+
+func (mw *OAuthMiddleware) handleOAuth2Callback(w http.ResponseWriter, r *http.Request) {
+	profile, err := mw.doOAuth2Roundtrip(w, r)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	} else {
+		// Mark logged in
+		sess := mw.webserver.GetSession(r)
+		sess.Values["profile"] = profile
+		sess.Save(r, w)
+
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
+}
+
+func (mw *OAuthMiddleware) doOAuth2Roundtrip(w http.ResponseWriter, r *http.Request) (*slack.User, error) {
+	code := r.FormValue("code")
+
+	token, err := mw.oauthCfg.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		log.Println("OAuth2: ", err)
+		return nil, fmt.Errorf("Error processing token.  Did you try to refresh ?")
+	}
+
+	//now get user data based on the Transport which has the token
+	client := mw.oauthCfg.Client(oauth2.NoContext, token)
+	//mw.bot.api.
+	resp, err := client.Get("https://www.googleapis.com/oauth2/v1/userinfo?alt=json")
+	if err != nil {
+		log.Fatal("Fatal error After OAuth2: ", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Could not read data from Google APIs: %s", err)
+	}
+
+	identity := struct {
+		UserID string `json:"user_id"`
+		URL    string
+		OK     bool
+	}{}
+	err = json.Unmarshal(body, &identity)
+	if err != nil {
+		return nil, fmt.Errorf("Could not read data from Google APIs: %s", err)
+	}
+	// TODO; do something with userid, fetch from our internal list.
+
+	if identity.URL != fmt.Sprintf("https://%s.slack.com") {
+		return nil, fmt.Errorf("Authenticated for wrong domain: %s", identity.URL)
+	}
+
+	return &slack.User{}, nil
+}

plotly/plotbot

(cherry picked from commit 54820624d8b01c726fd9e11bbfafedac6a62f53f)
Conflicts:
	bot.go
	example-bot/main.go
	web/session.go
	web/webapp.go

plotbot/main.go
@@ -16,6 +16,7 @@ import (
 	_ "github.com/plotly/plotbot/totw"
 	_ "github.com/plotly/plotbot/vote"
 	_ "github.com/plotly/plotbot/web"
+	_ "github.com/plotly/plotbot/webauth"
 	_ "github.com/plotly/plotbot/webutils"
 	_ "github.com/plotly/plotbot/wicked"
 )

plugin.go
@@ -1,6 +1,13 @@
 package plotbot
 
-import "github.com/gorilla/mux"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"github.com/nlopes/slack"
+)
 
 //
 // Bot plugins
@@ -13,15 +20,29 @@ type PluginInitializer interface {
 }
 
 type WebServer interface {
+	// Used internally by the `slick` library.
 	InitWebServer(*Bot, []string)
-	ServeWebRequests()
+	RunServer()
+
+	// Used by an Auth provider.
+	SetAuthMiddleware(func(http.Handler) http.Handler)
+	SetAuthenticatedUserFunc(func(req *http.Request) (*slack.User, error))
+
+	// Can be called by any plugins.
 	PrivateRouter() *mux.Router
 	PublicRouter() *mux.Router
+	GetSession(*http.Request) *sessions.Session
+	AuthenticatedUser(*http.Request) (*slack.User, error)
 }
 
 // WebPlugin initializes plugins with a `Bot` instance, a `privateRouter` and a `publicRouter`. All URLs handled by the `publicRouter` must start with `/public/`.
 type WebPlugin interface {
-	InitWebPlugin(*Bot, *mux.Router, *mux.Router)
+	InitWebPlugin(bot *Bot, private *mux.Router, public *mux.Router)
+}
+
+// WebServerAuth returns a middleware warpping the passed on `http.Handler`. Only one auth handler can be added.
+type WebServerAuth interface {
+	InitWebServerAuth(bot *Bot, webserver WebServer)
 }
 
 var registeredPlugins = make([]Plugin, 0)
@@ -56,9 +77,19 @@ func initWebPlugins(bot *Bot) {
 	}
 
 	for _, plugin := range registeredPlugins {
-		webPlugin, ok := plugin.(WebPlugin)
-		if ok {
+		if webPlugin, ok := plugin.(WebPlugin); ok {
 			webPlugin.InitWebPlugin(bot, bot.WebServer.PrivateRouter(), bot.WebServer.PublicRouter())
 		}
+
+		count := 0
+		if webServerAuth, ok := plugin.(WebServerAuth); ok {
+			count += 1
+
+			if count > 1 {
+				log.Fatalln("Can not load two WebServerAuth plugins. Already loaded one.")
+			}
+			webServerAuth.InitWebServerAuth(bot, bot.WebServer)
+		}
+
 	}
 }

web/user.go
@@ -1,29 +1,16 @@
 package web
 
 import (
-	"encoding/gob"
-	"html/template"
 	"encoding/json"
-	"log"
+	"html/template"
+
+	"github.com/nlopes/slack"
 )
 
-type GoogleUserProfile struct {
-	Name          string `json:"name"`
-	Hd            string `json:"hd"`
-	Email         string `json:"email"`
-	Picture       string `json:"picture"`
-	VerifiedEmail bool   `json:"verified_email"`
-}
-
-func (up *GoogleUserProfile) AsJavascript() template.JS {
-	jsonProfile, err := json.MarshalIndent(up, "", "  ")
+func userAsJavascript(user *slack.User) template.JS {
+	jsonProfile, err := json.MarshalIndent(user, "", "  ")
 	if err != nil {
-		log.Fatal("Couldn't marshal GoogleUserProfile for rendering", err)
-		return template.JS("{}")
+		return template.JS(`{"error": "couldn't decode user"}`)
 	}
 	return template.JS(jsonProfile)
-}
-
-func init() {
-	gob.Register(&GoogleUserProfile{})
 }

web/webapp.go
@@ -7,33 +7,30 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/GeertJohan/go.rice"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/context"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
 	"github.com/plotly/plotbot"
-	"golang.org/x/oauth2"
+	"github.com/nlopes/slack"
 )
 
 var web *Webapp
 
 type Webapp struct {
-	config         *WebappConfig
-	store          *sessions.CookieStore
-	bot            *plotbot.Bot
-	handler        *negroni.Negroni
-	privateRouter  *mux.Router
-	publicRouter   *mux.Router
-	enabledPlugins []string
+	config                *WebappConfig
+	store                 *sessions.CookieStore
+	bot                   *plotbot.Bot
+	handler               *negroni.Negroni
+	privateRouter         *mux.Router
+	publicRouter          *mux.Router
+	enabledPlugins        []string
+	authMiddleware        func(http.Handler) http.Handler
+	authenticatedUserFunc func(req *http.Request) (*slack.User, error)
 }
 
 type WebappConfig struct {
 	Listen            string `json:"listen"`
-	RedirectURL       string `json:"oauth_base_url"`
-	ClientID          string `json:"client_id"`
-	ClientSecret      string `json:"client_secret"`
-	RestrictDomain    string `json:"restrict_domain"`
 	SessionAuthKey    string `json:"session_auth_key"`
 	SessionEncryptKey string `json:"session_encrypt_key"`
 }
@@ -55,9 +52,6 @@ func (webapp *Webapp) InitWebServer(bot *plotbot.Bot, enabledPlugins []string) {
 	webapp.privateRouter = mux.NewRouter()
 	webapp.publicRouter = mux.NewRouter()
 
-	configureWebapp(&conf.Webapp)
-
-	webapp.privateRouter.HandleFunc("/", webapp.handleRoot)
 	web = webapp
 }
 
@@ -68,14 +62,34 @@ func (webapp *Webapp) PublicRouter() *mux.Router {
 	return webapp.publicRouter
 }
 
-func (webapp *Webapp) ServeWebRequests() {
+// SetAuthMiddleware should be called once by a WebServerAuth plugin, if any.
+func (webapp *Webapp) SetAuthMiddleware(middleware func(http.Handler) http.Handler) {
+	webapp.authMiddleware = middleware
+}
+
+// SetAuthenticatedUserFunc should be called once by a WebServerAuth plugin, if any.
+func (webapp *Webapp) SetAuthenticatedUserFunc(f func(req *http.Request) (*slack.User, error)) {
+	webapp.authenticatedUserFunc = f
+}
+
+func (webapp *Webapp) AuthenticatedUser(req *http.Request) (*slack.User, error) {
+	if webapp.authenticatedUserFunc == nil {
+		return nil, fmt.Errorf("No WebServerAuth plugin registered any AuthenticatedUser func call")
+	}
+	return webapp.authenticatedUserFunc(req)
+}
+
+func (webapp *Webapp) RunServer() {
 	privMux := http.NewServeMux()
-	privMux.Handle("/static/", http.StripPrefix("/static", http.FileServer(rice.MustFindBox("static").HTTPBox())))
-	privMux.Handle("/", webapp.privateRouter)
+	privMux.Handle("/", webapp.PrivateRouter())
 
 	pubMux := http.NewServeMux()
-	pubMux.Handle("/public/", webapp.publicRouter)
-	pubMux.Handle("/", NewOAuthMiddleware(privMux))
+	pubMux.Handle("/public/", webapp.PublicRouter())
+	if webapp.authMiddleware != nil {
+		pubMux.Handle("/", webapp.authMiddleware(privMux))
+	} else {
+		pubMux.Handle("/", privMux)
+	}
 
 	webapp.handler = negroni.Classic()
 	webapp.handler.UseHandler(context.ClearHandler(pubMux))
@@ -83,25 +97,13 @@ func (webapp *Webapp) ServeWebRequests() {
 	webapp.handler.Run(webapp.config.Listen)
 }
 
-// func LaunchWebapp(b *plotbot.Bot) {
-
-// 	rt.HandleFunc("/send_notif", handleNotif)
-// 	rt.HandleFunc("/hipchat/users", handleGetUsers)
-
-// 	n.Run("localhost:8080")
-// }
-
-func configureWebapp(conf *WebappConfig) {
-	oauthCfg = &oauth2.Config{
-		ClientID:     conf.ClientID,
-		ClientSecret: conf.ClientSecret,
-		RedirectURL:  conf.RedirectURL + "/oauth2callback",
-		Scopes:       []string{"openid", "profile", "email", "https://www.googleapis.com/auth/userinfo.profile"},
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  "https://accounts.google.com/o/oauth2/auth",
-			TokenURL: "https://accounts.google.com/o/oauth2/token",
-		},
+func (webapp *Webapp) GetSession(r *http.Request) *sessions.Session {
+	sess, err := web.store.Get(r, "plotbot")
+	if err != nil {
+		log.Println("web/session: warn: unable to decode Session cookie: ", err)
 	}
+
+	return sess
 }
 
 func (webapp *Webapp) handleRoot(w http.ResponseWriter, r *http.Request) {
@@ -110,7 +112,7 @@ func (webapp *Webapp) handleRoot(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	profile, _ := checkAuth(r)
+	profile, _ := webapp.AuthenticatedUser(r)
 
 	tpl, err := getRootTemplate()
 	if err != nil {
@@ -121,29 +123,21 @@ func (webapp *Webapp) handleRoot(w http.ResponseWriter, r *http.Request) {
 	ctx := struct {
 		CurrentUser, EnabledPlugins template.JS
 	}{
-		profile.AsJavascript(),
+		userAsJavascript(profile),
 		webapp.getEnabledPluginsJS(),
 	}
 	tpl.Execute(w, ctx)
 }
 
 func getRootTemplate() (*template.Template, error) {
-	box, err := rice.FindBox("static")
-	if err != nil {
-		return nil, fmt.Errorf("Error finding static assets: %s", err)
-	}
-
-	rawTpl, err := box.String("index.html")
-	if err != nil {
-		return nil, fmt.Errorf("Error loading index.html: %s", err)
-	}
-
-	tpl, err := template.New("index.html").Parse(rawTpl)
-	if err != nil {
-		return nil, fmt.Errorf("Cannot parse index.html: %s", err)
-	}
-
-	return tpl, nil
+	return template.New("index.html").Parse(`
+<html><head></head><body>
+<script>
+USER = {{.CurrentUser}};
+ENABLED_PLUGINS = {{.EnabledPlugins}};
+</script>
+</body></html>
+`)
 }
 
 func (webapp *Webapp) getEnabledPluginsJS() template.JS {

webauth/auth.go
@@ -1,0 +1,157 @@
+package webauth
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/nlopes/slack"
+	"github.com/plotly/plotbot"
+	"golang.org/x/oauth2"
+)
+
+func init() {
+	plotbot.RegisterPlugin(&OAuthPlugin{})
+}
+
+type OAuthPlugin struct {
+	config    OAuthConfig
+	webserver plotbot.WebServer
+}
+
+type OAuthConfig struct {
+	RedirectURL  string `json:"oauth_base_url"`
+	ClientID     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+}
+
+func (p *OAuthPlugin) InitWebServerAuth(bot *plotbot.Bot, webserver plotbot.WebServer) {
+	p.webserver = webserver
+
+	var config struct {
+		WebAuthConfig OAuthConfig
+	}
+	bot.LoadConfig(&config)
+
+	conf := config.WebAuthConfig
+	webserver.SetAuthMiddleware(func(handler http.Handler) http.Handler {
+		return &OAuthMiddleware{
+			handler:   handler,
+			plugin:    p,
+			webserver: webserver,
+			bot:       bot,
+			oauthCfg: &oauth2.Config{
+				ClientID:     conf.ClientID,
+				ClientSecret: conf.ClientSecret,
+				RedirectURL:  conf.RedirectURL + "/oauth2callback",
+				Scopes:       []string{"identify"},
+				Endpoint: oauth2.Endpoint{
+					AuthURL:  "https://slack.com/oauth/authorize",
+					TokenURL: "https://slack.com/api/oauth.access",
+				},
+			},
+		}
+	})
+}
+
+func (p *OAuthPlugin) AuthenticatedUser(r *http.Request) (*slack.User, error) {
+	sess := p.webserver.GetSession(r)
+
+	rawProfile, ok := sess.Values["profile"]
+	if ok == false {
+		return nil, fmt.Errorf("Not authenticated")
+	}
+	profile, ok := rawProfile.(*slack.User)
+	if ok == false {
+		return nil, fmt.Errorf("Profile data unreadable")
+	}
+	return profile, nil
+}
+
+type OAuthMiddleware struct {
+	handler   http.Handler
+	plugin    *OAuthPlugin
+	webserver plotbot.WebServer
+	oauthCfg  *oauth2.Config
+	bot       *plotbot.Bot
+}
+
+func (mw *OAuthMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/oauth2callback" {
+		mw.handleOAuth2Callback(w, r)
+		return
+	}
+
+	_, err := mw.plugin.AuthenticatedUser(r)
+	if err != nil {
+		if r.URL.Path == "/" {
+			log.Println("Not logged in", err)
+			url := mw.oauthCfg.AuthCodeURL("", oauth2.SetAuthURLParam("team", mw.bot.Config.TeamDomain))
+			http.Redirect(w, r, url, http.StatusFound)
+		} else {
+			w.WriteHeader(http.StatusForbidden)
+		}
+		return
+	}
+
+	// Check if session exists, yield a 403 unless we're on the main page
+	mw.handler.ServeHTTP(w, r)
+}
+
+func (mw *OAuthMiddleware) handleOAuth2Callback(w http.ResponseWriter, r *http.Request) {
+	profile, err := mw.doOAuth2Roundtrip(w, r)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	} else {
+		// Mark logged in
+		sess := mw.webserver.GetSession(r)
+		sess.Values["profile"] = profile
+		sess.Save(r, w)
+
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
+}
+
+func (mw *OAuthMiddleware) doOAuth2Roundtrip(w http.ResponseWriter, r *http.Request) (*slack.User, error) {
+	code := r.FormValue("code")
+
+	token, err := mw.oauthCfg.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		log.Println("OAuth2: ", err)
+		return nil, fmt.Errorf("Error processing token.  Did you try to refresh ?")
+	}
+
+	//now get user data based on the Transport which has the token
+	client := mw.oauthCfg.Client(oauth2.NoContext, token)
+	//mw.bot.api.
+	resp, err := client.Get("https://www.googleapis.com/oauth2/v1/userinfo?alt=json")
+	if err != nil {
+		log.Fatal("Fatal error After OAuth2: ", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Could not read data from Google APIs: %s", err)
+	}
+
+	identity := struct {
+		UserID string `json:"user_id"`
+		URL    string
+		OK     bool
+	}{}
+	err = json.Unmarshal(body, &identity)
+	if err != nil {
+		return nil, fmt.Errorf("Could not read data from Google APIs: %s", err)
+	}
+	// TODO; do something with userid, fetch from our internal list.
+
+	if identity.URL != fmt.Sprintf("https://%s.slack.com") {
+		return nil, fmt.Errorf("Authenticated for wrong domain: %s", identity.URL)
+	}
+
+	return &slack.User{}, nil
+}