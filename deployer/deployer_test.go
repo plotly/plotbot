4a83235eb0fafe3f50b53fa06db88e1314c776e4
bpostlethwaite <post.ben.here@gmail.com>
2017-04-17 13:20:17 -0400

    test lock and unlock
    Also add another defaultDeployer that doesn't set a mock cmd delay

--- deployer/deployer_test.go
+++ deployer/deployer_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/plotly/plotbot/testutils"
 )
 
-func newTestDep(dconf DeployerConfig, bot plotbot.BotLike) *Deployer {
+func newTestDep(dconf DeployerConfig, bot plotbot.BotLike, runner Runnable) *Deployer {
 
 	execPath, err := os.Executable()
 	if err != nil {
@@ -46,27 +46,30 @@ func newTestDep(dconf DeployerConfig, bot plotbot.BotLike) *Deployer {
 	}
 
 	return &Deployer{
-		config: &defaultdconf,
-		bot:    bot,
-		runner: &testutils.MockRunner{
+		config:   &defaultdconf,
+		bot:      bot,
+		runner:   runner,
+		progress: make(chan string, 1000),
+	}
+}
+
+func defaultTestDep(cmdDelay time.Duration) *Deployer {
+	return newTestDep(
+		DeployerConfig{},
+		testutils.NewDefaultMockBot(),
+		&testutils.MockRunner{
 			ParseVars: func(c string, s ...string) []string {
 				switch c {
 				case "ansible-playbook":
 					return []string{
 						"GO_CMD_PROCESS_OUTPUT={{ansible-output}}",
-						"GO_CMD_PROCESS_DELAY=1",
+						fmt.Sprintf("GO_CMD_PROCESS_DELAY=%d", cmdDelay/time.Second),
 					}
 				default:
 					return []string{}
 				}
 			},
-		},
-		progress: make(chan string, 1000),
-	}
-}
-
-func defaultTestDep() *Deployer {
-	return newTestDep(DeployerConfig{}, testutils.NewDefaultMockBot())
+		})
 }
 
 func captureProgress(dep *Deployer, waitTime time.Duration) (testutils.Searchable, error) {
@@ -77,7 +80,7 @@ func captureProgress(dep *Deployer, waitTime time.Duration) (testutils.Searchabl
 	for {
 		select {
 		case <-timer.C:
-			return nil, fmt.Errorf("timer expired without progress")
+			return progress, fmt.Errorf("timer expired without progress")
 		case <-done:
 			return progress, nil
 		case p := <-dep.progress:
@@ -126,7 +129,7 @@ func TestCmdProcess(t *testing.T) {
 }
 
 func TestCancelDeployNotRunning(t *testing.T) {
-	dep := defaultTestDep()
+	dep := defaultTestDep(time.Second)
 
 	conv := plotbot.Conversation{
 		Bot: dep.bot,
@@ -148,7 +151,7 @@ func TestCancelDeployNotRunning(t *testing.T) {
 }
 
 func TestStageDeploy(t *testing.T) {
-	dep := defaultTestDep()
+	dep := defaultTestDep(time.Second)
 
 	conv := plotbot.Conversation{
 		Bot: dep.bot,
@@ -204,7 +207,7 @@ func TestStageDeploy(t *testing.T) {
 }
 
 func TestProdDeployWithTags(t *testing.T) {
-	dep := defaultTestDep()
+	dep := defaultTestDep(time.Second)
 
 	conv := plotbot.Conversation{
 		Bot: dep.bot,
@@ -246,3 +249,107 @@ func TestProdDeployWithTags(t *testing.T) {
 	}
 }
 
+func TestLockUnlock(t *testing.T) {
+
+	// First test locking - set command delay to 0 so we can wait for progress
+	// on a shorter interval.
+	dep := defaultTestDep(time.Second * 0)
+
+	conv := plotbot.Conversation{
+		Bot: dep.bot,
+	}
+
+	msg := testutils.ToBotMsg(dep.bot, "please lock deployment")
+	dep.ChatHandler(&conv, &msg)
+
+	// there should be no progress
+	_, err := captureProgress(dep, time.Millisecond*500)
+	if err == nil {
+		t.Errorf("expected timout error while capturing non-existent progress")
+	}
+
+	runner := dep.runner.(*testutils.MockRunner)
+	if len(runner.Jobs) != 0 {
+		t.Fatalf("expected no job to be run found %d", len(runner.Jobs))
+	}
+
+	bot := dep.bot.(*testutils.MockBot)
+	if len(bot.TestReplies) != 1 {
+		t.Fatalf("expected 1 replies found %d", len(bot.TestReplies))
+	}
+
+	actual := bot.TestReplies[0].Text
+	expected := "Deployment is now locked"
+	if !strings.Contains(actual, expected) {
+		t.Fatalf("exected '%s' to contain '%s'", expected, actual)
+	}
+
+	// Then make sure a deploy fails while locked
+	clearMocks(dep)
+	conv = plotbot.Conversation{
+		Bot: dep.bot,
+	}
+
+	msg = testutils.ToBotMsgFromUser(dep.bot, "deploy to prod", "rodoh")
+	dep.ChatHandler(&conv, &msg)
+
+	_, err = captureProgress(dep, time.Millisecond*500)
+	if err == nil {
+		t.Errorf("expected timout error while capturing non-existent progress")
+	}
+
+	if len(runner.Jobs) != 0 {
+		t.Fatalf("expected no job to be run found %d", len(runner.Jobs))
+	}
+
+	if len(bot.TestReplies) != 1 {
+		t.Fatalf("expected 1 replies found %d", len(bot.TestReplies))
+	}
+
+	actual = bot.TestReplies[0].Text
+	expected = fmt.Sprintf("Deployment was locked by %s", testutils.DefaultFromUser)
+	if !strings.Contains(actual, expected) {
+		t.Fatalf("exected '%s' to contain '%s'", expected, actual)
+	}
+
+	// Unlock deployment
+	clearMocks(dep)
+	conv = plotbot.Conversation{
+		Bot: dep.bot,
+	}
+
+	msg = testutils.ToBotMsg(dep.bot, "unlock deployment")
+	dep.ChatHandler(&conv, &msg)
+
+	_, err = captureProgress(dep, time.Millisecond*500)
+	if err == nil {
+		t.Errorf("expected timout error while capturing non-existent progress")
+	}
+
+	if len(runner.Jobs) != 0 {
+		t.Fatalf("expected no job to be run found %d", len(runner.Jobs))
+	}
+
+	if len(bot.TestReplies) != 1 {
+		t.Fatalf("expected 1 replies found %d", len(bot.TestReplies))
+	}
+
+	actual = bot.TestReplies[0].Text
+	expected = "Deployment is now unlocked"
+	if !strings.Contains(actual, expected) {
+		t.Fatalf("exected '%s' to contain '%s'", expected, actual)
+	}
+
+	// Finally make sure we can now deploy
+	conv = plotbot.Conversation{
+		Bot: dep.bot,
+	}
+	msg = testutils.ToBotMsg(dep.bot, "deploy to prod")
+
+	dep.ChatHandler(&conv, &msg)
+	captureProgress(dep, time.Millisecond*500)
+
+	if len(runner.Jobs) != 3 {
+		t.Fatalf("expected 3 job found %d", len(runner.Jobs))
+	}
+}
